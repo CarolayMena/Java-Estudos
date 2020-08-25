f56da5504952895841a8fdfba08ae819a3f4a1e4
Janett Mena <carolaymena@hotmail.com>
2020-08-25 08:19:46 -0300

    ajuste if

--- Programa.java
+++ Programa.java
@@ -3,11 +3,10 @@ public class Programa {
 	public static void main ( String[] args ) {
 
 		String nome = "Janett";
-		if( nome == "Janett") 
-		     {
-		System.out.println("Hello world");
-	 	     }else {
-		System.out.println("Não é a Janett");
-		     }
-}
+		if( nome.equals("Janett") ) {
+			System.out.println("Hello world");
+	 	} else {
+			System.out.println("Não é a Janett");
+		}
+	}
 }
