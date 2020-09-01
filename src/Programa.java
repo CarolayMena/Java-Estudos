57393570900f08f06f117a82ce498d456e3dd9f5
Janett Mena <carolaymena@hotmail.com>
2020-09-01 08:59:38 -0300

    Laço for

--- src/Programa.java
+++ src/Programa.java
@@ -4,7 +4,11 @@ public class Programa {
 
 	public static void main ( String[] args ) {
 		Scanner scanner = new Scanner(System.in);
-		String input = scanner.next();
-		System.out.println(input);
+		System.out.println( "Digite um numero: " );
+		int input = scanner.nextInt();
+		System.out.println( "O numero digitado foi: " + input);
+		for ( int i=0; i<input; i++ ) {
+			System.out.println( "Valor do input: " + (i + 1) );
+		}
 	}
 }
