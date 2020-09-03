6166f47e99eba5dee72196204ddced044050906e
Janett Mena <carolaymena@hotmail.com>
2020-09-03 08:04:41 -0300

    while no README.md 02/09/20

--- src/Programa.java
+++ src/Programa.java
@@ -3,12 +3,23 @@ import java.util.Scanner;
 public class Programa {
 
 	public static void main ( String[] args ) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println( "Digite um numero: " );
-		int input = scanner.nextInt();
-		System.out.println( "O numero digitado foi: " + input);
-		for ( int i=0; i<input; i++ ) {
-			System.out.println( "Valor do input: " + (i + 1) );
-		}
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println( "Digite um numero: ");
+//		int input = scanner.nextInt();
+		int contador =0;
+			while (contador <4){
+				contador++;
+//				System.out.println("valor do contador" + contador);
+
+//				if(contador ==3) {
+//					break;
+//				}
+
+				if(contador==2 || contador==4) {
+				continue;
+				}
+			 System.out.println("valor do contador" + contador);
+
+			}
 	}
 }
