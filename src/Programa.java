f83b2636a2264e3a293fdea3e79f172021168dc4
Janett Mena <carolaymena@hotmail.com>
2020-09-03 08:48:41 -0300

    do while 03/09/20

--- src/Programa.java
+++ src/Programa.java
@@ -4,7 +4,26 @@ public class Programa {
 
 	public static void main ( String[] args ) {
 		Scanner scanner = new Scanner(System.in);
-		String input = scanner.next();
-		System.out.println(input);
+		int input1=0;
+		int input2=0;
+		int menu=0;
+		do {
+			System.out.println("Escolha uma opcao 1-Soma 2-Subtracao 0-Sair");
+			menu = scanner.nextInt();
+			if (menu==1){
+	                        System.out.println("Digite um numero1");
+				input1 = scanner.nextInt();
+				System.out.println("Digite um numero2");
+				input2 = scanner.nextInt();
+	                        System.out.println("soma " + (input1+input2));
+		      	} else if (menu==2){
+	                        System.out.println("Digite um numero1");
+                                input1 = scanner.nextInt();
+                                System.out.println("Digite um numero2");
+                                input2 = scanner.nextInt();
+                                System.out.println("subtracao" + (input1-input2));
+                        }
+
+		} while( menu!=0 );
 	}
 }
