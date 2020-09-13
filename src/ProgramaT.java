cf62e7bf94d4c237880221208fe659dbdfbba401
Janett Mena <carolaymena@hotmail.com>
2020-09-13 11:29:52 -0300

    usando for

--- src/ProgramaT.java
+++ src/ProgramaT.java
@@ -3,15 +3,15 @@ import java.util.ArrayList;
 
 public class ProgramaT {
 	public static void main (String[] args) {
-		List<Funcionario> lista = new ArrayList<>();
-		Funcionario fun = new Funcionario("Carolay", 36);
-                Funcionario fun2 = new Funcionario("Mena", 46);
-		lista.add(new Funcionario("Janett", 36));
-		lista.add(fun);
-		lista.add(fun2);
-		for (int i=0; i<lista.size() ; i++ ) {
-			 System.out.println(lista.get(i).nome + " " + lista.get(i).idade);
-		}
+		List lista  = new ArrayList<>();
+		String nome;
+		int idade;
+		int tel;
+
+		lista.add("Carolay");
+		lista.add(36);
+		lista.add(345678);
+		System.out.println(lista);
 
 	}
 }
